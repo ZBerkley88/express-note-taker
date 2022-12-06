5ca924b702dcaa97f1701b558e42c6ca738eaa36
Zack <zachary.berkley@gmail.com>
2022-12-06 12:47:33 -0500

    Uploading to Heroku

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 // helper method for generating unique ids
 const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 var cors = require('cors');
 
 const app = express();
